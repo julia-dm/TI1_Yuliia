f7e4e1625b7968068e7684765f3f955c947dd66a
julia-dm <yulia.2508dmitruk@gmail.com>
2026-04-08 10:52:04 +0200

    Add btn to small screens add gallery and lightbox

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,5 @@
-function open_menu(){
-   
-}
+const btn=document.getElementById("open_menu_btn");
+const nav_mobile=document.getElementById("nav_mobile")
+btn.addEventListener("click",function(){
+    nav_mobile.classList.toggle('open');
+})
